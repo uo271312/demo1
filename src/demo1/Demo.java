78e0baa71aee9bee0a056685e993385e41d149eb
usuario <usuario@DESKTOP-QHEN8Q6>
2020-09-29 14:06:10 +0200

    cambio2

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -3,5 +3,9 @@ package demo1;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+		
+		
 	}
 }
